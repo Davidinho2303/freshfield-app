81211ff3cca562f78e2294ef0951c58285ba6a3e
Davidinho2303 <snaak@hotmail.de>
2026-03-08 20:09:10 +0100

    Update route.ts

--- freshfield/app/auth/callback/route.ts
+++ freshfield/app/auth/callback/route.ts
@@ -2,23 +2,14 @@ import { createClient } from '@/lib/supabase/server'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
-  const token_hash = searchParams.get('token_hash')
-  const type = searchParams.get('type') as any
-  const next = searchParams.get('next') ?? '/feed'
-
-  const supabase = createClient()
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+  const next = requestUrl.searchParams.get('next') ?? '/feed'
 
   if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
-    if (!error) return NextResponse.redirect(`${origin}${next}`)
+    const supabase = createClient()
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  if (token_hash && type) {
-    const { error } = await supabase.auth.verifyOtp({ token_hash, type })
-    if (!error) return NextResponse.redirect(`${origin}${next}`)
-  }
-
-  return NextResponse.redirect(`${origin}/auth/error`)
+  return NextResponse.redirect(`${requestUrl.origin}${next}`)
 }
